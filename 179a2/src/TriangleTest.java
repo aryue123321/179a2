88d8905ff76c4b4e50b856ca51568628ad99b816
Chun Yue Leung <aryue123321@gmail.com>
2019-02-16 14:23:59 -0800

    Started getAreaTest

--- 179a2/src/TriangleTest.java
+++ 179a2/src/TriangleTest.java
@@ -12,7 +12,14 @@ class TriangleTest {
 		assertEquals(true, new Triangle(3,1,1).isImpossible(), "length a > b+c is not a triangle");
 		assertEquals(true, new Triangle(1,3,1).isImpossible(), "length b > a+c is not a triangle");
 		assertEquals(true, new Triangle(1,1,3).isImpossible(), "length c > a+b is not a triangle");
-
+		assertEquals(true, new Triangle(-1,-1,-1).isImpossible(), "all negative lengths is not a triangle");
+		assertEquals(false, new Triangle(3,4,5).isImpossible(), "length 3 4 5 is an triangle");
+	}
+	
+	@Test
+	void getAreaTest() {
+		assertEquals(-1, new Triangle(0,0,0).getArea(), "isImpossible gives area -1");
+		assertEquals(6, new Triangle(3,4,5).getArea(), "length 3 4 5 has area 6");
 	}
 
 }
