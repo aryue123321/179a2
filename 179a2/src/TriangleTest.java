8609145bf6eae16c1893d560893eac5f6de892e3
Chun Yue Leung <aryue123321@gmail.com>
2019-02-16 14:14:46 -0800

    test for the impossible property

--- 179a2/src/TriangleTest.java
+++ 179a2/src/TriangleTest.java
@@ -8,7 +8,10 @@ class TriangleTest {
 	void isImpossibleTest() {
 		assertEquals(true, new Triangle(0,0,0).isImpossible(), "A point is not a triangle");
 		assertEquals(true, new Triangle(1,1,2).isImpossible(), "A line is not a triangle");
-		assertEquals(false, new Triangle(1,1,1).isImpossible(), "Invalid traingle with lengths all zeros ");
+		assertEquals(true, new Triangle(-1,1,1).isImpossible(), "A triangle cannot has negative length");
+		assertEquals(true, new Triangle(3,1,1).isImpossible(), "length a > b+c is not a triangle");
+		assertEquals(true, new Triangle(1,3,1).isImpossible(), "length b > a+c is not a triangle");
+		assertEquals(true, new Triangle(1,1,3).isImpossible(), "length c > a+b is not a triangle");
 
 	}
 
