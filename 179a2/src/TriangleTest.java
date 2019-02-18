21b91e56d18271bd218b1ffef38aaa6419a6dc75
Chun Yue Leung <aryue123321@gmail.com>
2019-02-18 02:02:17 -0800

    fix bugs on perimeters test

--- 179a2/src/TriangleTest.java
+++ 179a2/src/TriangleTest.java
@@ -15,11 +15,15 @@ class TriangleTest {
 		return (a*a + b*b == c*c) || (b*b + c*c == a*a) || (a*a + c*c == b*b);
 	}
 	
+	static long caculatePerimter(long a, long b, long c) {
+    return a + b + c;
+  }
+	
 	@Nested
 	class InvalidTriangleTest{
 		
 		@ParameterizedTest(name = "s1 = {0}, s2 = {1}, s3 = {2}")
-		@CsvSource(value = {"0,0,0", "-1,-1,-1", "-1,2,3", "1,1,2", "-3,-4,-5", "3,1,1", "2147483647,2147483647,0"})
+		@CsvSource(value = {"0,0,0", "-1,-1,-1", "-1,2,3", "1,1,2", "-3,-4,-5", "3,1,1", "2147483647,2147483647,0", "1,1,0"})
 		void properties_of_invalid_triangle(int s1, int s2, int s3) {
 			Triangle t = new Triangle(s1,s2,s3);
 			assertAll(
@@ -46,7 +50,7 @@ class TriangleTest {
 			assertAll(
 				() -> assertEquals("equilateral", t.classify(), "equilateral classifies as isosceles"),
 				() -> assertEquals(TriangleTest.calculateArea(s1, s2, s3), t.getArea(), " equilateral has area same as calculated"),
-				() -> assertEquals(s1+s2+s3, t.getPerimeter(), "equilateral perimeter"),
+				() -> assertEquals(TriangleTest.caculatePerimter(s1, s2, s3), t.getPerimeter(), "equilateral perimeter"),
 				() -> assertEquals(String.format("%d,%d,%d", s1, s2, s3), t.getSideLengths(), "equilateral return input lengths in string"),
 				() -> assertTrue(t.isEquilateral(), "equilateral is equilateral"),
 				() -> assertFalse(t.isImpossible(), "equilateral is not impossible"),
@@ -60,9 +64,8 @@ class TriangleTest {
 	
 	@Nested
 	class PureIsoscelesTriangleTest{
-		
 		@ParameterizedTest(name = "s1 = {0}, s2 = {1}, s3 = {2}")
-		@CsvSource(value = {"2,2,1", "1,2,2", "2,1,2", "2147483647, 2147483647, 1"})
+		@CsvSource(value = {"2,2,1", "1,2,2", "2,1,2", "2147483647, 2147483647, 1", "1, 2147483647, 2147483647", "2147483647, 1, 2147483647"})
 		void properties_of_isosceles(int s1, int s2, int s3) {
 			Triangle t = new Triangle(s1,s2,s3);
 			assertAll(
@@ -71,7 +74,7 @@ class TriangleTest {
 					 */
 				() -> assertEquals("isossceles", t.classify(), "pure isosceles classifies as isosceles"),
 				() -> assertEquals(TriangleTest.calculateArea(s1, s2, s3), t.getArea(), " pure isosceles area has area same as calculated"),
-				() -> assertEquals(s1+s2+s3, t.getPerimeter(), "pure isosceles perimeter"),
+				() -> assertEquals(TriangleTest.caculatePerimter(s1, s2, s3), t.getPerimeter(), "pure isosceles perimeter"),
 				() -> assertEquals(String.format("%d,%d,%d", s1, s2, s3), t.getSideLengths(), "return input lengths in string"),
 				() -> assertFalse(t.isEquilateral(), "pure isosceles is not equilateral"),
 				() -> assertFalse(t.isImpossible(), "pure isosceles is not impossible"),
@@ -92,12 +95,12 @@ class TriangleTest {
 			assertAll(
 				() -> assertEquals("right-angled", t.classify(), "right angled classifies as right angled"),
 				() -> assertEquals(TriangleTest.calculateArea(s1, s2, s3), t.getArea(), "right angled has area same as calculated"),
-				() -> assertEquals(s1+s2+s3, t.getPerimeter(), "equilateral perimeter"),
+				() -> assertEquals(TriangleTest.caculatePerimter(s1, s2, s3), t.getPerimeter(), "equilateral perimeter"),
 				() -> assertEquals(String.format("%d,%d,%d", s1, s2, s3), t.getSideLengths(), "right angled return input lengths in string"),
 				() -> assertFalse(t.isEquilateral(), "right angled is not equilateral"),
 				() -> assertFalse(t.isImpossible(), "right angled is not impossible"),
 				() -> assertFalse(t.isIsosceles(), "right angled is isosceles"),
-				() -> assertEquals(TriangleTest.isRightAngled(s1, s2, s3), t.isRightAngled(), "right angled is right angled"),
+				() -> assertEquals(true, t.isRightAngled(),"right angled is right angled"),
 				() -> assertFalse(t.isScalene(), "right angled is not scalene")
 			);
 		}
@@ -107,22 +110,23 @@ class TriangleTest {
 	class CloseToRightAngledTest{
 		
 		@ParameterizedTest(name = "s1 = {0}, s2 = {1}, s3 = {2}")
-		@CsvSource(value = {"1295743693, 1295743692, 1832458304"})
+		@CsvSource(value = {"1295743693, 1295743692, 1832458304", "2147483647, 2147483646, 2"})
 		void properties_of_close_to_right_angled(int s1, int s2, int s3) {
 			Triangle t = new Triangle(s1,s2,s3);
 			assertAll(
 				() -> assertEquals("scalene", t.classify(), "close to right angled classifies as scalene"),
 				() -> assertEquals(TriangleTest.calculateArea(s1, s2, s3), t.getArea(), "close to right angled has area same as calculated"),
-				() -> assertEquals(s1+s2+s3, t.getPerimeter(), "equilateral perimeter"),
+				() -> assertEquals(TriangleTest.caculatePerimter(s1, s2, s3), t.getPerimeter(), "equilateral perimeter"),
 				() -> assertEquals(String.format("%d,%d,%d", s1, s2, s3), t.getSideLengths(), " close to right angled return input lengths in string"),
 				() -> assertFalse(t.isEquilateral(), "close to right angled is not equilateral"),
 				() -> assertFalse(t.isImpossible(), "close to right angled is not impossible"),
 				() -> assertFalse(t.isIsosceles(), "close to right angled is not isosceles"),
-				() -> assertEquals(TriangleTest.isRightAngled(s1, s2, s3), t.isRightAngled(), "close to right angled is not right angled"),
+				() -> assertEquals(false, t.isRightAngled(), "close to right angled is not right angled"),
 				() -> assertTrue(t.isScalene(), "close to right angled is scalene")
 			);
 		}
 	}
+	
 	
 
 }
